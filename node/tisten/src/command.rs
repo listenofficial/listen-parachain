3ec9116e7aee2cbc014c045ce36ccdb486fa335a
JimYam <askbaba1@protonmail.com>
2022-06-22 18:25:04 +0800

    client updrade to polkadot-v0.9.24

--- node/tisten/src/command.rs
+++ node/tisten/src/command.rs
@@ -56,42 +56,7 @@ impl SubstrateCli for Cli {
 
 	fn support_url() -> String {
 		"https://github.com/listenofficial/listen-parachain/issues/new".into()
-	}	fn impl_name() -> String {
-		"Listen Node".into()
 	}
-
-	fn impl_version() -> String {
-		env!("SUBSTRATE_CLI_IMPL_VERSION").into()
-	}
-
-	fn description() -> String {
-		"Listen Node\n\nThe command-line arguments provided first will be \
-		passed to the parachain node, while the arguments provided after -- will be passed \
-		to the relay chain node.\n\n\
-		parachain-collator <parachain-args> -- <relay-chain-args>"
-			.into()
-	}
-
-	fn author() -> String {
-		env!("CARGO_PKG_AUTHORS").into()
-	}
-
-	fn support_url() -> String {
-		"https://github.com/listenofficial/listen-parachain/issues/new".into()
-	}
-
-	fn copyright_start_year() -> i32 {
-		2020
-	}
-
-	fn load_spec(&self, id: &str) -> std::result::Result<Box<dyn sc_service::ChainSpec>, String> {
-		load_spec(id)
-	}
-
-	fn native_runtime_version(_: &Box<dyn ChainSpec>) -> &'static RuntimeVersion {
-		&parachain_template_runtime::VERSION
-	}
-
 
 	fn copyright_start_year() -> i32 {
 		2020

6459772c1043d628ba509b0caf41a1694ae66885
JimYam <askbaba1@protonmail.com>
2022-06-30 10:57:23 +0800

    update kico Para_ID for testing

--- runtime/tisten/src/lib.rs
+++ runtime/tisten/src/lib.rs
@@ -132,7 +132,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("listen-parachain"),
 	impl_name: create_runtime_str!("listen-parachain"),
 	authoring_version: 1,
-	spec_version: 2022062901,
+	spec_version: 2022063001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
